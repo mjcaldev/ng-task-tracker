a2c62e5329ef656443e6b58e6e53ce52aaa34354
Micheal Callaghan <mehaul96@gmail.com>
2025-11-09 15:13:55 -0500

    set provideHttp in config, taskboardpage in routes, and task interface

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TaskBoardPage } from './features/tasks/pages/task-board.page'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TaskBoardPage},
+];
---
+++ src/app/features/tasks/pages/task-board.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-task-board-page',
+  imports: [],
+  template: `
+  <section style="padding:1rem">
+    <h1>Task Board</h1>
+    <p>Placeholder page. We'll add the board UI next.</p>
+  </section>
+  `,
+})
+
+export class TaskBoardPage {
+  
+}
